47c0868152aa0e2755f599b722f288f0a1107d8e
jbfink <john.fink@gmail.com>
2011-06-07 20:45:25 -0400

    I'm so dumb, I forgot how to convert a range to an array.

--- 1/one.rb
+++ 1/one.rb
@@ -1,6 +1,9 @@
 #If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 #
 #Find the sum of all the multiples of 3 or 5 below 1000.
+# here's what I do, because I am bad at math:
+# Define a range (1 to 1000)
+# slice everything out of that range divided by three or five with a modulo of zero, put that in another range, and add it up
 
-range = [1..1000]
+range = (1..1000).to_a
 
