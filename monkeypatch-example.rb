7bd0669606b5a2d0d93a5a29883eabaf5dfc5ec7
jbfink <john.fink@gmail.com>
2012-01-03 18:11:46 -0500

    Another monkeypatch example.

--- monkeypatch-example.rb
+++ monkeypatch-example.rb
@@ -6,3 +6,12 @@ p "Hello" + " " + self
 end
 end
 
+#another example -- use this to make an is_prime? maybe?
+class Integer
+   def is_even?
+  return true if self == 2
+  return true if self % 2 == 0
+  return false
+  end
+end
+
