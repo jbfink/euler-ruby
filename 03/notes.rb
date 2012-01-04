cea248ed221bf73b9d3325594291433c1cd7f95a
jbfink <john.fink@gmail.com>
2012-01-04 08:20:38 -0500

    Note about count method.

--- 03/notes.rb
+++ 03/notes.rb
@@ -8,5 +8,6 @@ end
 factors.each do |count|
 if count == 0 then sum += 1
 
-# so if sum == 2 then number is prime
+
+# so if sum == 2 then number is prime, but could also use factors.count(0) too
 #so what I want to do is take an array of 1..n and for *each* thing do this test where *those factors themselves* are split into factors, and if their sum of the modulos is *exactly* two (viz, divisible by themselves and by 1) then push *that* into an array, and then read that array[-1] to get the last factor. dang.
