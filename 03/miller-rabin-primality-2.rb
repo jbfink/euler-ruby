4ee89c742f0d029fad4866886b4887bfd736afe5
jbfink <john.fink@gmail.com>
2012-01-04 21:57:45 -0500

    add another M-R primality from http://www.dreamincode.net/code/snippet6420.htm

---
+++ 03/miller-rabin-primality-2.rb
@@ -0,0 +1,55 @@
+K = 20
+
+def is_probably_prime?(n)
+    #fast calculation of base^exponent % modulus
+    def modular_pow(base, exponent, modulus)
+        result = 1
+        while exponent > 0 do
+            if (exponent & 1) == 1 then
+               result = (result * base) % modulus
+            end
+            exponent = exponent >> 1
+            base = (base * base) % modulus
+        end
+        return result
+    end
+
+    if n < 5 then
+        if n == 2 || n == 3 then
+            return true
+        else
+            return false
+        end
+    end
+
+    #writing n-1 as 2^s * d (d odd)
+    s = 0
+    d = n - 1
+    while (d%2 == 0) do
+        d=d/2
+        s=s+1
+    end
+
+    K.times do
+        a = Random.rand(n-4) + 2 #random number on (2,n-2)
+        x = modular_pow(a,d,n)
+        if (x==1 || x==n-1) then 
+            next
+        end
+        for r in 1 .. s-1 do
+            x = modular_pow(x,2,n)
+            if x == 1 then
+                return false
+            end
+            if x == n-1 then
+                stop = true
+                break;
+            end
+        end
+        if (stop) then
+            next;
+        end
+        return false
+    end
+    return true;
+end
