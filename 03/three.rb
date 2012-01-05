b03d0c5de071c9bd0e3729a939b41482e7c82e3d
jbfink <john.fink@gmail.com>
2012-01-04 21:08:26 -0500

    Add lamentation to three.rb.

--- 03/three.rb
+++ 03/three.rb
@@ -1,11 +1,19 @@
 #The prime factors of 13195 are 5, 7, 13 and 29.
 #
 #What is the largest prime factor of the number 600851475143 ?
+#note: the below sucks. It works for small numbers, but is damn nigh useless
+#for anything large. Dammit. 
+
 class Integer
-def divisors
-return (1..self).to_a
-end
-def is_prime?
-#code goes here
-end
-end
+      def is_prime?
+      return true if self == 1
+          array = []
+          (1..self).to_a.each do |i|
+                array.push(self % i)
+       end
+           if array.count(0) == 2 then return true
+       else return false
+       end
+     end
+   end
+
